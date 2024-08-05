2e7ed329f9c35bb9d7ea127a7e75a9dda9602238
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-05 13:50:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{ static void Main(string[] args)
+    {
+        MakeSomeGarbge();
+        Console.WriteLine($"Memory used Before Collection{GC.GetTotalMemory(false):N0}");
+        GC.Collect();
+        Console.WriteLine($"Memory used after collection:{GC.GetTotalMemory (true):N0}");
+    }
+    static void MakeSomeGarbge()
+    {
+        Version v;
+        for (int i = 0; i < 1000; i++)
+        {
+            v = new Version();
+        }
+    }
+  /*  private static void Main(string[] args)
+      {
+     var p =new Person();
+        p.Name = "Ali";
+    }
+}
+class Person
+{ public string Name
+    { get; set; } 
+
+    public Person()
+    {
+        Console.WriteLine("This is persont constructure"); 
+    }
+     ~Person()
+    {
+        Console.WriteLine("This is persont constructure");
+    }*/
+}
